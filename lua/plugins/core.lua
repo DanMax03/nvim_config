033b33e7c72d2ee32777b93a7bcf8df079102172
Daniil Maximov <daniil31415it@gmail.com>
2024-03-03 23:05:50 +0300

    Added neo-tree plugin

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -11,6 +11,15 @@ return {
       end
    },
    {
+      'nvim-neo-tree/neo-tree.nvim',
+      branch = 'v3.x',
+      dependencies = {
+         'nvim-lua/plenary.nvim',
+         'nvim-tree/nvim-web-devicons',
+         'MunifTanjim/nui.nvim'
+      }
+   },
+   {
       'nvim-telescope/telescope.nvim',
       tag = '0.1.5',
       dependencies = { 'nvim-lua/plenary.nvim' }
