b7a56ec8f4a957c2d1af73dc81eea2533fd9660f
Daniil Maximov <daniil31415it@gmail.com>
2024-03-04 01:44:25 +0300

    Added volatile folder

--- lua/plugins/volatile.lua
+++ lua/plugins/volatile.lua
@@ -1,10 +1,16 @@
 local g = vim.g
+local table_helper = require('helpers/table').table
 
 
 local result = {}
 
+local function addPlugin (require_path)
+   result = table_helper.concat_tables(result, require(require_path))
+end
+
+
 if g.is_idris2_setup then
-   table.insert(result, require('plugins/idris2'))
+   addPlugin('plugins/volatile/idris2')
 end
 
 return result
