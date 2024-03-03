702cf88d4993e581d0bd3dc7a104dac856779303
Daniil Maximov <daniil31415it@gmail.com>
2024-03-03 22:56:27 +0300

    Added kanagawa theme

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,5 +1,16 @@
 return {
    {
+      'rebelot/kanagawa.nvim',
+      lazy = false,  -- ensuring installation during startup
+      priority = 1000,  -- ensuring it goes first
+      opts = {
+         theme = "wave"
+      },
+      config = function()
+         vim.cmd("colorscheme kanagawa")
+      end
+   },
+   {
       'nvim-telescope/telescope.nvim',
       tag = '0.1.5',
       dependencies = { 'nvim-lua/plenary.nvim' }
