1dd959f7d46606f5144cc7cdc8fc613edeb24d41
Daniil Maximov <daniil31415it@gmail.com>
2024-03-03 23:06:30 +0300

    Allowed 24-bit colors, added some comments

--- lua/base/view.lua
+++ lua/base/view.lua
@@ -1,11 +1,12 @@
 local opt = vim.opt
 
 
-opt.guicursor = ""
+opt.guicursor = ""  -- let the cursor stay always a filled rectangle
+opt.termguicolors = true  -- enable 24-bit colors
 
-opt.number = true
+opt.number = true  -- adding line numbers on the left side of the window
 
 opt.cursorline = true
 opt.cursorcolumn = true
 
-opt.wrap = false
+opt.wrap = false  -- don't wrap my lines!
