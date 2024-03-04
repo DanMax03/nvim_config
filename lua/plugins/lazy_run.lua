8a44569614235d357edf61b9ff89ddd9fd54f19c
Daniil Maximov <daniil31415it@gmail.com>
2024-03-04 21:43:49 +0300

    Added some output

--- lua/plugins/lazy_run.lua
+++ lua/plugins/lazy_run.lua
@@ -2,6 +2,11 @@ local core_plugins = require('plugins/core')
 local volatile_plugins = require('plugins/volatile')
 local plugins = require('helpers').table.concat_tables(core_plugins, volatile_plugins)
 
+print('plugins are:')
+for _, plugin in ipairs(plugins) do
+   print(plugin[0])
+end
+
 require('lazy').setup({
    plugins
 })
