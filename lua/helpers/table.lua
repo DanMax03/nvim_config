b85ab65a09f396632a68b259e7b73285e5abf823
Daniil Maximov <daniil31415it@gmail.com>
2024-03-04 22:44:03 +0300

    Updated output

--- lua/helpers/table.lua
+++ lua/helpers/table.lua
@@ -14,9 +14,11 @@ function M.concatTables(...)
    local arg = {...}
    local result = {}
 
-   for i, t in ipairs(arg) do
+   for _, t in ipairs(arg) do
       if M.getTableLength(t) == 0 then
+         print('skipping table')
          goto continue
+         print('this must never show up')
       end
 
       assert(type(elem) == 'table', 'Every argument to concat_tables must be a table!')
