fe9e4a93a70ef8c15372f14f5f20ec0aa337f10a
Daniil Maximov <daniil31415it@gmail.com>
2024-03-07 23:21:11 +0300

    Added dependency for telescope

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -39,7 +39,7 @@ return {
    {
       'nvim-telescope/telescope.nvim',
       tag = '0.1.5',
-      dependencies = { 'nvim-lua/plenary.nvim' }
+      dependencies = { 'nvim-lua/plenary.nvim', 'BurntSushi/ripgrep' }
    },
    {
       'ThePrimeagen/harpoon',
