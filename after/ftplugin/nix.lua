c178ad504c5ab5f16a937af617e58c57e35b2175
Daniil Maximov <daniil31415it@gmail.com>
2024-03-07 18:10:52 +0300

    Removed debug output, moved ftplugin to after/ folder

---
+++ after/ftplugin/nix.lua
@@ -0,0 +1,2 @@
+vim.opt_local.shiftwidth = 10
+vim.opt_local.tabstop = 10
--- lua/plugins/lazy_run.lua
+++ lua/plugins/lazy_run.lua
@@ -5,12 +5,7 @@ local core_plugins = require('plugins/core')
 local volatile_plugins = require('plugins/volatile')
 local plugins = table_helpers.concatTables(core_plugins, volatile_plugins)
 
-print('Before lazy')
-print(vim.inspect(vim.api.nvim_list_runtime_paths()))
-
 require('lazy').setup(plugins, {
    defaults = { lazy = true }  -- always go lazy if is not explicit
 })
 
-print('After lazy')
-print(vim.inspect(vim.api.nvim_list_runtime_paths()))
