a6daef551a0b7bff94c32902064ff31aa2d07b89
Daniil Maximov <daniil31415it@gmail.com>
2024-03-08 00:06:41 +0300

    Added some formatting

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -39,7 +39,8 @@ return {
    {
       'nvim-telescope/telescope.nvim',
       tag = '0.1.5',
-      dependencies = { 'nvim-lua/plenary.nvim', 'BurntSushi/ripgrep' }
+      dependencies = { 'nvim-lua/plenary.nvim', 'BurntSushi/ripgrep' },
+      cmd = "Telescope"
    },
    {
       'ThePrimeagen/harpoon',
--- lua/plugins/lazy_run.lua
+++ lua/plugins/lazy_run.lua
@@ -6,6 +6,8 @@ local volatile_plugins = require('plugins/volatile')
 local plugins = table_helpers.concatTables(core_plugins, volatile_plugins)
 
 require('lazy').setup(plugins, {
-   defaults = { lazy = true }  -- always go lazy if is not explicit
+   defaults = {
+      lazy = true  -- always go lazy if is not explicit
+   }
 })
 
