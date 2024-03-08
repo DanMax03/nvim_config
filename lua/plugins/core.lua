89b40e37e9efe0208f1d2e81c171c3ad25fbf7ad
Daniil Maximov <daniil31415it@gmail.com>
2024-03-08 11:06:00 +0300

    Added hijacking netrw by neo-tree

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -54,6 +54,24 @@ return {
          'nvim-tree/nvim-web-devicons',
          'MunifTanjim/nui.nvim'
       },
+      init = function()
+         vim.api.nvim_create_autocmd('BufEnter', {
+            group = vim.api.nvim_create_autogroup('NeoTreeInit', {clear = true}),
+            callback = function()
+               local f = vim.fn.expand('%:p')
+
+               if vim.fn.isdirectory(f) ~= 0 then
+                  vim.cmd('Neotree current dir=' .. f)
+                  vim.api.nvim_clear_autocmds({group = 'NeoTreeInit'})
+               end
+            end,
+         })
+      end,
+      opts = {
+         filesystem = {
+            hijack_netrw_behavior = 'open_current'
+         }
+      },
       cmd = 'Neotree',
    },
    {
