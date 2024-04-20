ebd2edc7432f097a76ff0a7627ce04a260f91c1f
Daniil Maximov <daniil31415it@gmail.com>
2024-04-20 19:06:20 +0300

    Added diagnostic toggle

--- lua/keys/core.lua
+++ lua/keys/core.lua
@@ -4,3 +4,15 @@
 
 --vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 --vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+vim.api.nvim_create_user_command('DiagnosticToggle', function()
+   local dconfig = vim.diagnostic.config
+   local vt_flag = dconfig().virtual_text
+
+   dconfig({
+      virtual_text = not vt_flag,
+      underline = not vt_flag,
+      signs = not vt_flag
+   })
+end, { desc = 'Toggle diagnostic' })
+
